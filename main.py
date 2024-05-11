a13012762a355394ed23759e1ba5138faefd8308
Viraj <virajmohit7@gmail.com>
2024-05-11 04:57:21 -0700

    Problem Statement Feature

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import streamlit as st
+import json
+from streamlit_js_eval import streamlit_js_eval
 import openai
 
 st.title('Prompt Study Tool')
 
-client = openai.Client(api_key=st.secrets["OPENAI_API_KEY"])
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 if "openai_model" not in st.session_state:
     st.session_state.openai_model = "gpt-3.5-turbo"
@@ -11,17 +13,42 @@ if "openai_model" not in st.session_state:
 if "messages" not in st.session_state:
     st.session_state.messages = []
     
+if "topic" not in st.session_state:
+    st.session_state.topic = ""
 
+if "chapter_numbers" not in st.session_state:
+    st.session_state.chapter_numbers = []
 
 st.header("Topic:", divider="violet")
-topic = st.text_area("Enter the topic you want to study", key="topic")
+if topic := st.text_area("Enter the topic you want to study"):
+    st.session_state.topic = topic
+    response = openai.chat.completions.create(
+        model=st.session_state["openai_model"],
+        response_format={ 'type': 'json_object'},
+        messages=[
+            {"role": "system", "content": "You are a course textbook writing expert designed to output in JSON."},
+            {"role": "user", "content": "Generate the table of contents (include chapter and sections) for a textbook on " + st.session_state.topic + "."},
+        ],
+        seed=138,
+        temperature=0.1,
+    )
+    data = json.loads(response.choices[0].message.content)
+    print(data)
+    chapters = data['table_of_contents']['chapters']
+    st.session_state.chapter_numbers = [str(chapter['chapter_number']) + ': ' + chapter['chapter_title'] for chapter in chapters]
+    print(st.session_state.chapter_numbers)
 
-
-st.header("Reading + Problems", divider="violet")
-st.markdown("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed consectetur urna, quis mollis erat. Vestibulum congue velit non aliquam ullamcorper. Proin arcu nibh, varius id finibus at, iaculis sit amet diam. Donec arcu tellus, semper at vestibulum id, ultricies eget ex. Proin egestas tempor metus, lobortis laoreet lacus congue id. Aenean luctus mauris id feugiat varius. Aenean nisl ex, aliquet id molestie non, ornare congue nulla. Vestibulum euismod, nisi eget lacinia pretium, nunc metus bibendum magna, eu ullamcorper ex lectus vel enim.")
+    st.header("Reading + Problems", divider="violet")
+    chapter_tabs = st.tabs(st.session_state.chapter_numbers)
+    for chapter_tab, chapter in zip(chapter_tabs, chapters):
+        with chapter_tab:
+            for section in chapter['sections']:
+                st.subheader(section)
+                st.markdown("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed consectetur urna, quis mollis erat. Vestibulum congue velit non aliquam ullamcorper. Proin arcu nibh, varius id finibus at, iaculis sit amet diam. Donec arcu tellus, semper at vestibulum id, ultricies eget ex. Proin egestas tempor metus, lobortis laoreet lacus congue id. Aenean luctus mauris id feugiat varius. Aenean nisl ex, aliquet id molestie non, ornare congue nulla. Vestibulum euismod, nisi eget lacinia pretium, nunc metus bibendum magna, eu ullamcorper ex lectus vel enim.")
 
 with st.sidebar:
-    chat_area = st.container(height=800, border=True)
+    st.header("General Chat", divider="rainbow")
+    chat_area = st.container(height=int(streamlit_js_eval(js_expressions='screen.height', key = 'SCR')*0.5),border=True)
     
     for message in st.session_state.messages:
         with chat_area:
@@ -36,12 +63,9 @@ with st.sidebar:
                 st.markdown(prompt)
                 
             with st.chat_message("assistant"):
-                stream = client.chat.completions.create(
+                stream = openai.chat.completions.create(
                     model=st.session_state["openai_model"],
-                    messages=[
-                        {"role": m["role"], "content": m["content"]}
-                        for m in st.session_state.messages
-                    ],
+                    messages=st.session_state.messages,
                     stream=True,
                 )
                 response = st.write_stream(stream)
