9cff7c2857af09df8fe9e3d3539bc44a6486d88b
Viraj <virajmohit7@gmail.com>
2024-05-13 11:41:02 -0700

    fixed requirements.txt

--- main.py
+++ main.py
@@ -149,6 +149,36 @@ def code(problem, form_id):
             st.code(problem["Correct_code"])
 
 
+screen_width = streamlit_js_eval(js_expressions="screen.height", key="SCR")
+
+
+@st.experimental_fragment
+def chat_area():
+    st.header("General Chat", divider="rainbow")
+    chat_area = st.container(height=int(screen_width * 0.5), border=True)
+
+    for message in st.session_state.messages:
+        with chat_area:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+    #     with chat_message_placeholder.chat_message(message["role"]):
+    #         chat_message_placeholder.markdown(message["content"])
+    if prompt := st.chat_input("hello there"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with chat_area:
+            with st.chat_message("user"):
+                st.markdown(prompt)
+
+            with st.chat_message("assistant"):
+                stream = openai.chat.completions.create(
+                    model=st.session_state["openai_model"],
+                    messages=st.session_state.messages,
+                    stream=True,
+                )
+                response = st.write_stream(stream)
+            st.session_state.messages.append({"role": "assistant", "content": response})
+
+
 # if st.button("Clear"):
 #     st.write("Clearing state...")
 
@@ -161,8 +191,9 @@ def code(problem, form_id):
 # test()
 st.header("Topic:", divider="violet")
 topic = st.text_area("Enter the topic you want to study")
-screen_width = streamlit_js_eval(js_expressions="screen.height", key="SCR")
 if topic != st.session_state.topic and topic is not None and topic != "":
+    with st.sidebar:
+        chat_area()
     st.session_state.topic = topic
     example = r"""{
         "table_of_contents": {
@@ -339,32 +370,6 @@ if topic != st.session_state.topic and topic is not None and topic != "":
                         problem["testcases"] = correct_code["testcases"]
                         code(problem, form_id)
 
-    with st.sidebar:
-        st.header("General Chat", divider="rainbow")
-        chat_area = st.container(height=int(screen_width * 0.5), border=True)
-
-        for message in st.session_state.messages:
-            with chat_area:
-                with st.chat_message(message["role"]):
-                    st.markdown(message["content"])
-        #     with chat_message_placeholder.chat_message(message["role"]):
-        #         chat_message_placeholder.markdown(message["content"])
-        if prompt := st.chat_input("hello there"):
-            st.session_state.messages.append({"role": "user", "content": prompt})
-            with chat_area:
-                with st.chat_message("user"):
-                    st.markdown(prompt)
-
-                with st.chat_message("assistant"):
-                    stream = openai.chat.completions.create(
-                        model=st.session_state["openai_model"],
-                        messages=st.session_state.messages,
-                        stream=True,
-                    )
-                    response = st.write_stream(stream)
-                st.session_state.messages.append(
-                    {"role": "assistant", "content": response}
-                )
 
 # if prompt := st.chat_input("What is up?"):
 #     st.session_state.messages.append({"role": "user", "content": prompt})
